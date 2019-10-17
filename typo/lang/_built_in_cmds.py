35537f0250df22a397dcfcc23614174266d45457
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2019-10-17 15:29:12 -0500

    Add more built_in_cmds to Typo Script

--- typo/lang/_built_in_cmds.py
+++ typo/lang/_built_in_cmds.py
@@ -10,6 +10,17 @@ def do_explodedict(dictionary, name):
     _call_typo_func('assignment', '%scount'%name, len(dictionary))
     for (i, item) in dictionary.items():
         _call_typo_func('assignment', '%s %s'%(name, i), item)
+def do_openfile(name, mode):
+    return open(name, mode)
+def do_filewrite(file, data, location=None):
+    if 'b' in file.mode:
+        data = bytes(data)
+    if location is not None:
+        file.seek(location)
+    file.write(data)
+def do_fileseek(file, location, from_=0):
+    file.seek(location, from_)
 
 register_import.register_func('explodelist', do_explodelist, 2)
 register_import.register_func('explodedict', do_explodedict, 2)
+register_import.register_func('file open', do_openfile, 1, 2, 'r')
